14e76d1768b843031a25ef7cfd5de103687949b0
Chu Tuấn Kiệt <105277832+kaolaa20@users.noreply.github.com>
2024-03-09 16:24:21 +0700

    Config addToCart to create a new BookingEntity(Undone))

--- CarRentalPlatform/Pages/AddToCart.cshtml.cs
+++ CarRentalPlatform/Pages/AddToCart.cshtml.cs
@@ -11,18 +11,23 @@ namespace CarRentalPlatform.Pages
     {
         private readonly ICarRepository _carRepo;
         private readonly IBookingRepository _bookingRepo;
+        private readonly IAccountRepository _accountRepo;
 
-        public AddToCartModel(ICarRepository carRepo, IBookingRepository bookingRepo)
+        public AddToCartModel(ICarRepository carRepo, IBookingRepository bookingRepo, IAccountRepository accountRepo)
         {
             _carRepo = carRepo;
             _bookingRepo = bookingRepo;
+            _accountRepo = accountRepo;
         }
 
         public CarEntity CarEntity { get; set; } = default!;
 
+        public AccountEntity Customer { get; set; } = default!;
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             var cars = await _carRepo.GetAllCars();
+            var cus = await _accountRepo.GetAccountById(1);
             if (id == null || cars == null)
             {
                 return NotFound();
@@ -35,21 +40,40 @@ namespace CarRentalPlatform.Pages
             }
             else
             {
-                CarEntity = carentity;
+                CarEntity = carentity;                
             }
+
+            TempData["CarId"] = id;
             return Page();
         }
 
         public BookingEntity BookingEntity { get; set; }
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(BookingEntity bookingEntity)
         {
-            if (BookingEntity == null)
+            if (!ModelState.IsValid)
             {
-                throw new ArgumentNullException(nameof(BookingEntity), "Entity cannot be null.");
+                return Page();
             }
-            //BookingEntity.CustomerId =
-            await _bookingRepo.CreateBooking(BookingEntity);
-            return Page();
+            if (!TempData.TryGetValue("CarId", out var carIdObj) || !(carIdObj is int id))
+            {
+                return NotFound();
+            }
+            var carEntity = await _carRepo.GetCarById(id);
+
+            if (carEntity == null)
+            {
+                return NotFound();
+            }
+
+            // Update bookingEntity with form data and carEntity
+            bookingEntity.Car = carEntity;
+
+            bookingEntity.StartDate = BookingEntity.StartDate;
+            bookingEntity.EndDate = BookingEntity.EndDate;
+
+            await _bookingRepo.CreateBooking(bookingEntity);
+
+            return RedirectToPage("/ShoppingCart");
         }
     }
 }
