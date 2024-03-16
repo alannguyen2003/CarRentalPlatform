2f41e6eddfdf60912503c6d40a53b7eb736bf3e8
quoctm17 <tranminhquoc0711@gmail.com>
2024-03-16 22:58:32 +0700

    commit Flow

--- CarRentalPlatform/Pages/CustomerPage/ReturnCar.cshtml.cs
+++ CarRentalPlatform/Pages/CustomerPage/ReturnCar.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Repository.Abstract;
 
 namespace CarRentalPlatform.Pages.CustomerPage
 {
     public class ReturnCarModel : PageModel
     {
-        public void OnGet()
+        private readonly IBookingRepository _bookingRepository;
+
+        public ReturnCarModel(IBookingRepository bookingRepository)
         {
+            _bookingRepository = bookingRepository;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            await _bookingRepository.UpdateBookingStatus(id, 3);
+            return RedirectToPage("./BookingHistory");
         }
     }
+
 }
