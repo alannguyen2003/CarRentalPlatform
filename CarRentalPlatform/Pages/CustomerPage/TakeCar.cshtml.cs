b2cdd88d197c27afa586a4551a5bcbf2c375c8f4
quoctm17 <tranminhquoc0711@gmail.com>
2024-03-16 22:42:05 +0700

    Add Update Booking for Customer

---
+++ CarRentalPlatform/Pages/CustomerPage/TakeCar.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Repository.Abstract;
+
+namespace CarRentalPlatform.Pages.CustomerPage
+{
+    public class TakeCarModel : PageModel
+    {
+        private readonly IBookingRepository _bookingRepository;
+        public TakeCarModel(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            await _bookingRepository.UpdateBookingStatus(id, 2);
+            return RedirectToPage("/BookingHistory");
+        }
+    }
+}
