9f5d7a6059e4ea150376b1790ff108ea440e26bf
quoctm17 <tranminhquoc0711@gmail.com>
2024-03-25 19:23:11 +0700

    Add SeedCar

--- CarRentalPlatform/Pages/CustomerPage/PayTotalAmount.cshtml.cs
+++ CarRentalPlatform/Pages/CustomerPage/PayTotalAmount.cshtml.cs
@@ -27,7 +27,7 @@ namespace CarRentalPlatform.Pages.CustomerPage
             if (account.WalletBalance >= booking.TotalAmount)
             {
                 // Deduct the amount from the wallet and update
-                account.WalletBalance -= booking.TotalAmount.Value;
+                account.WalletBalance -= booking.TotalAmount;
                 await _accountRepository.UpdateAccount(account);
 
                 // Optionally, update booking status to Completed or Paid
