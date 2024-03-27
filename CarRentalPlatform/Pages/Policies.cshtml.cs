6969610c13b29496676a585de0f5491a655eccb3
quoctm17 <tranminhquoc0711@gmail.com>
2024-03-27 15:31:25 +0700

    Add Policies & Term

---
+++ CarRentalPlatform/Pages/Policies.cshtml.cs
@@ -0,0 +1,24 @@
+using CarRentalPlatform.Configuration;
+using DataTransferLayer.DataTransfer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarRentalPlatform.Pages
+{
+    public class PoliciesModel : PageModel
+    {
+        [BindProperty]
+        public bool IsLogin { get; set; }
+        public AccountDto Account { get; set; }
+        public IActionResult OnGet()
+        {
+            IsLogin = SessionHelper.GetObjectFromJson<bool>(HttpContext.Session, "isLogin");
+            Account = SessionHelper.GetObjectFromJson<AccountDto>(HttpContext.Session, "user");
+            if (IsLogin == false)
+            {
+                return RedirectToPage("./login");
+            }
+            return null;
+        }
+    }
+}
